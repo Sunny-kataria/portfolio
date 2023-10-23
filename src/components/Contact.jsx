fd7321fa160cdde162036f78b71a6087778329f0
Saggy <katariasunny86@gmail.com>
2023-10-23 09:10:41 +0530

    .

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
 
         <div className=" flex justify-center items-center">
           <form
-            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            action="https://getform.io/f/e3774cee-3434-446c-8885-643345306062"
             method="POST"
             className=" flex flex-col w-full md:w-1/2"
           >
