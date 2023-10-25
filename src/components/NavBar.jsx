56d891ce6642278412ea484ece64540901342cfd
Saggy <katariasunny86@gmail.com>
2023-10-25 22:26:59 +0530

    updating portfolio

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,11 +16,11 @@ const NavBar = () => {
     },
     {
       id: 3,
-      link: "portfolio",
+      link: "project",
     },
     {
       id: 4,
-      link: "experience",
+      link: "technology",
     },
     {
       id: 5,
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,38 +1,125 @@
+// import React from "react";
+// import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
+// import installNode from "../assets/portfolio/installNode.jpg";
+// import navbar from "../assets/portfolio/navbar.jpg";
+// import reactParallax from "../assets/portfolio/reactParallax.jpg";
+// import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+// import reactWeather from "../assets/portfolio/reactWeather.jpg";
+
+// const Portfolio = () => {
+//   const portfolios = [
+//     {
+//       id: 1,
+//       src: arrayDestruct,
+//     },
+//     {
+//       id: 2,
+//       src: reactParallax,
+//     },
+//     {
+//       id: 3,
+//       src: navbar,
+//     },
+//     {
+//       id: 4,
+//       src: reactSmooth,
+//     },
+//     {
+//       id: 5,
+//       src: installNode,
+//     },
+//     {
+//       id: 6,
+//       src: reactWeather,
+//     },
+//   ];
+
+//   return (
+//     <div
+//       name="portfolio"
+//       className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+//     >
+//       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+//         <div className="pb-8">
+//           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+//             Portfolio
+//           </p>
+//           <p className="py-6">Check out some of my work right here</p>
+//         </div>
+
+//         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+//           {portfolios.map(({ id, src }) => (
+//             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+//               <img
+//                 src={src}
+//                 alt=""
+//                 className="rounded-md duration-200 hover:scale-105"
+//               />
+//               <div className="flex items-center justify-center">
+//                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+//                   Demo
+//                 </button>
+//                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+//                   Code
+//                 </button>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Portfolio;
+
+
 import React from "react";
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
+import tictactoe from "../assets/portfolio/tictactoe.jpg";
+import password from "../assets/portfolio/password.jpg";
 import installNode from "../assets/portfolio/installNode.jpg";
 import navbar from "../assets/portfolio/navbar.jpg";
 import reactParallax from "../assets/portfolio/reactParallax.jpg";
 import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
 import reactWeather from "../assets/portfolio/reactWeather.jpg";
 
+const portfolios = [
+  {
+    id: 1,
+    src: tictactoe,
+    githubUrl: "https://github.com/Sunny-kataria/js-project-/tree/master/Tic%20Tac%20Toe"
+  },
+  {
+    id: 2,
+    src: password,
+    githubUrl: "https://github.com/Sunny-kataria/js-project-/tree/master/password%20Generator"
+  },
+  {
+    id: 3,
+    src: navbar,
+    githubUrl: "https://github.com/Sunny-kataria/navbar-project"
+  },
+  {
+    id: 4,
+    src: reactSmooth,
+    githubUrl: "https://github.com/Sunny-kataria/react-smooth-scroll-project"
+  },
+  {
+    id: 5,
+    src: installNode,
+    githubUrl: "https://github.com/Sunny-kataria/install-node-project"
+  },
+  {
+    id: 6,
+    src: reactWeather,
+    githubUrl: "https://github.com/Sunny-kataria/js-project-/tree/master/whether%20app"
+  },
+];
+
 const Portfolio = () => {
-  const portfolios = [
-    {
-      id: 1,
-      src: arrayDestruct,
-    },
-    {
-      id: 2,
-      src: reactParallax,
-    },
-    {
-      id: 3,
-      src: navbar,
-    },
-    {
-      id: 4,
-      src: reactSmooth,
-    },
-    {
-      id: 5,
-      src: installNode,
-    },
-    {
-      id: 6,
-      src: reactWeather,
-    },
-  ];
+  const redirectToGitHub = (githubUrl) => {
+    window.open(githubUrl, "_blank");
+  };
 
   return (
     <div
@@ -42,24 +129,24 @@ const Portfolio = () => {
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            Portfolio
+             My projects
           </p>
-          <p className="py-6">Check out some of my work right here</p>
+          <p className="py-6">Check out some of my project right here</p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, githubUrl }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
                 alt=""
-                className="rounded-md duration-200 hover:scale-105"
+                className="rounded-md duration-200 hover:scale-105 h-60 object-cover"
               />
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                <button
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                  onClick={() => redirectToGitHub(githubUrl)}
+                >
                   Code
                 </button>
               </div>
@@ -72,3 +159,5 @@ const Portfolio = () => {
 };
 
 export default Portfolio;
+
+
